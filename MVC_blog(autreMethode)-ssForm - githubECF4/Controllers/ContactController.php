a1be22170a4db48128d85bdf32b02c3efa0a758c
LAZREG <ticemlazreg83@gmail.com>
2026-03-20 09:17:46 +0100

    feat: ajout de la page contact et de son controleur

---
+++ MVC_blog(autreMethode)-ssForm - githubECF4/Controllers/ContactController.php
@@ -0,0 +1,6 @@
+<?php
+class ContactController extends Controller {
+public function index() {
+$this->render('contact'); 
+    }
+}
---
+++ MVC_blog(autreMethode)-ssForm - githubECF4/Views/contact.php
@@ -0,0 +1,7 @@
+<h1>Contactez-moi</h1>
+<form action="" method="POST">
+    <input type="text" name="nom" placeholder="Votre nom" required><br>
+    <input type="email" name="email" placeholder="Votre email" required><br>
+    <textarea name="message" placeholder="Votre message"></textarea><br>
+    <button type="submit">Envoyer</button>
+</form>
